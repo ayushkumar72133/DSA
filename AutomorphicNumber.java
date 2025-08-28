34dc5795a806d44cc6b57c8872759719faead650
Ayush kumar <ayushkumar72133@gmail.com>
2025-08-28 23:16:53 +0530

    AutomorphicNumber

---
+++ AutomorphicNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AutomorphicNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int square = num * num;
+        String numStr = String.valueOf(num);
+        String squareStr = String.valueOf(square);
+        
+        if (squareStr.endsWith(numStr)) {
+            System.out.println(num + " is an Automorphic Number");
+        } else {
+            System.out.println(num + " is not an Automorphic Number");
+        }
+    }
+}
